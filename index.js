49bcf776bb814b476373e6edfc428c49ee73e3f8
ramapithecus <92436221+ramapithecus@users.noreply.github.com>
2021-10-13 23:16:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+ function f1() {
+ var y = document.getElementById('a').value;
+ var x = document.getElementById('result');
+    x.innerHTML = y;
+}
+ function f2() {
+ var y = document.getElementById('b').value;
+ var x = document.getElementById('tank');
+    x.innerHTML = y;
+}
+function f3() {
+ var y = document.getElementById('x').value;
+ var k = document.getElementById('rus');
+    k.innerHTML = y;
+}
